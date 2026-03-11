e9ff50fb33ea40c0aa8e57efe8691faad6e96f60
Arthur <arthursgz77@gmail.com>
2026-03-11 11:52:52 -0300

    Adjust media calculation to remove rounding
    Removed decimal precision from media calculation.

--- calcularNota.py
+++ calcularNota.py
@@ -2,7 +2,7 @@ n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 n3 = float(input("Digite a terceira nota: "))
 
-media = round((n1 + n2 + n3) / 3, 2)
+media = round((n1 + n2 + n3) / 3)
 
 if media < 5:
     print("Reprovado")
